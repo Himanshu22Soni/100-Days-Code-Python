b5773503443b7ffc44473bb6b962ebc2a789104f
Himanshu Soni <sonihimanshu2210@gmail.com>
2022-12-08 11:27:22 +0530

    Day 7 and 8 Completed

---
+++ Day 7&8.py
@@ -0,0 +1,18 @@
+# Basic Calculator
+
+a = 75
+b = 8
+add = a+b
+print("Addition is ", add)
+sub = a-b
+print("Subtraction is ", sub)
+mul = a*b
+print("Multiplication is ", mul)
+div = a/b
+print("Division is ", div)
+fdiv = a//b
+print("Floor Division is ", fdiv)
+mod = a % b
+print("Modulus is ", mod)
+exp = a**b
+print("Exponential is ", exp)
