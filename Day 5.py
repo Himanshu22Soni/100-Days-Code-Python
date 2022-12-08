e05f335342cc8150576304a55755c2832b796069
Himanshu Soni <sonihimanshu2210@gmail.com>
2022-12-08 11:26:26 +0530

    Day 5 Completed

---
+++ Day 5.py
@@ -0,0 +1,23 @@
+# Comments can be added by either ctrl+/ or we can make it by ''' this also.
+
+'''
+print('Hello World')
+print('Please Complete the code')
+
+'''
+
+# Escape sequences Characters
+
+# Here \n is changing line for me without pressing enter.
+print('Hello Himanshu Soni\n How are you')
+
+# \" \" is used to place a "" on a string inside a string.
+print('Hello World \"Himanshu Soni\" is author of this code')
+
+# More on Print Statements
+
+# Here seperator or sep is seperating all the parameters by "~" we can give any sep. Default is ' '.
+print('Hello World', 6, 9, sep="~")
+
+# Here end is defining that parameter will be printed and after that end value will be printed.End can be null also. Default is "\n"
+print('Hello', end=" Himanshu Soni")
