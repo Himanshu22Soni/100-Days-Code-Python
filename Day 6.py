5c00408a658757ca9df7f0e5af25376957518344
Himanshu Soni <sonihimanshu2210@gmail.com>
2022-12-08 11:26:47 +0530

    Day 6 Completed

---
+++ Day 6.py
@@ -0,0 +1,21 @@
+# Data Types and Variables.
+
+# Variables hold data.
+
+a = 1
+print(a)  # Number can be int, float or complex
+print(complex(2, 3))
+
+b = True
+print(b)  # Boolean
+
+c = "Himanshu"
+print(c)  # String
+
+d = None
+print(d)  # None
+
+# Type Checking.
+print("The type of a,b,c,d are", type(a), type(b), type(c), type(d))
+
+# Other types are tuple,dictionary,list discussed later.
