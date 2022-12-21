2e6a4d27a5616552461b94a56fec387eb00d7ef6
Himanshu Soni <sonihimanshu2210@gmail.com>
2022-12-21 20:39:57 +0530

    Day 24 Completed

---
+++ Day 24.py
@@ -0,0 +1,11 @@
+# Tuples in Python.
+
+# Most basic difference between tuple and list is that List can be modified and tuples can not be modified and changed.
+tup1 = (1, 2, 3, 4, 5, 6, 7, "Himanshu")
+
+# If we are creating a tuple with a single element, we need to add a comma at the end. It is mandatory.
+tup2 = (1,)
+
+# Rest almost every concept is same like List. For example, Range, slicing, negative indexes etc.
+
+print(tup1)
