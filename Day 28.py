8bc62b20e04a595386d438b501ca9ff010a02401
Himanshu Soni <sonihimanshu2210@gmail.com>
2022-12-26 21:15:24 +0530

    Day 28 Completed

---
+++ Day 28.py
@@ -0,0 +1,10 @@
+# f strings in Python.
+
+name = "Himanshu Soni"
+country = "India"
+
+# To print variables in between string we use f string like this.
+print(f"Hi my name is {name} and my country is {country}")
+
+# To print string as it is, not replacing variable still we can use f string like this.
+print(f"Hi my name is {{name}} and my country is {{country}}")
