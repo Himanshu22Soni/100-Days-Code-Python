1afeabceb6f0a8fdc3c5e7db4c7f43a1b42ade20
Himanshu Soni <sonihimanshu2210@gmail.com>
2023-01-12 15:54:30 +0530

    Day 44 Completed

---
+++ Day 44.py
@@ -0,0 +1,11 @@
+# Imports in Python.
+
+import math
+from math import pi  # Can import one variable or function also.
+from math import sqrt as sq
+
+print(sq(2))
+print(pi)
+
+# This will tell me all the variables and functions present in imported library.
+print(dir(math))
